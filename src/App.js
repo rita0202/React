af7217abce9a9cc1fa2670d7e516e87e42d85806
rita0202 <140054917+rita0202@users.noreply.github.com>
2023-11-02 15:02:44 +0600

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Portfolio from './components/Portfolio';
+import styles from './App.module.css'
+
+function App() {
+  return (
+    <div className={styles.wrapper}>
+      <Portfolio />
+    </div>
+  
+  );
+}
+
+export default App;
